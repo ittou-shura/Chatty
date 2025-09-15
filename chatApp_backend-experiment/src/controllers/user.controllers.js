acbd3f3d1b41e71d2310609a617932730bda0c7d
sujata13574 <sujata13574@gmail.com>
2025-09-15 22:18:09 +0000

    feat: filter current user from users list

--- chatApp_backend-experiment/src/controllers/user.controllers.js
+++ chatApp_backend-experiment/src/controllers/user.controllers.js
@@ -1,9 +1,11 @@
 import User from "../models/user.models.js";
 
 export const getUsers = async (req, res) => {
-  const currentUserId = req.userId;
   try {
-    const users = await User.find({ _id: { $ne: currentUserId } }, { password: 0 });
+    const loggedInUserId = req.user._id;
+
+    const users = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: "Could not get users." });
--- chatApp_backend-experiment/src/routes/user.routes.js
+++ chatApp_backend-experiment/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { getPublicKey, getUsers } from "../controllers/user.controllers.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", getUsers);
+router.get("/", protectRoute, getUsers);
 router.get("/public-key/:userId", getPublicKey);
 
 export default router;
